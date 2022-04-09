391ab34de7e0217a7a95f10d29be266738f54b5f
Omer Khan <omerkhan.nu@gmail.com>
2022-04-09 10:08:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@AnwaarUlHassan/main.cpp
@@ -0,0 +1,16 @@
+// Given an array of characters, determine if the array is a palindrome or not
+
+// Example
+// Input: arr = "redivider"
+// Output: True
+
+// Example
+// Input: arr = "zambia"
+// Output: False
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+  
+}
