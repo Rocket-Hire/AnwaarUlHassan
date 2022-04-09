14cfc5620cac1adef54cbc798e26ffb58a4c7585
Omer Khan <omerkhan.nu@gmail.com>
2022-04-09 11:11:40 +0000

    Solved it correctly using multiple approaches. Knows about different types of joins

--- main.cpp
+++ main.cpp
@@ -5,63 +5,40 @@ int main() {
   std::cout << "Hello World!\n";
 
 }
+// Write an SQL query to find the employees who earn more than their managers.
 
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Return the result table in any order.
 
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// The query result format is in the following example.
 
-// You can return the answer in any order.
-
-// Example 1:
-
-// Input: nums = [2,7,11,15], target = 9
-// Output: [0,1]
-// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
-
-// Input: nums = [3,2,4], target = 6
-// Output: [1,2]
-// Example 3:
-
-// Input: nums = [3,3], target = 6
-// Output: [0,1]Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-// You can return the answer in any order.
+ 
 
 // Example 1:
 
-// Input: nums = [2,7,11,15], target = 9
-// Output: [0,1]
-// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
+// Input: 
+// Employee table:
+// +----+-------+--------+-----------+
+// | id | name  | salary | managerId |
+// +----+-------+--------+-----------+
+// | 1  | Joe   | 70000  | 3         |
+// | 2  | Henry | 80000  | 4         |
+// | 3  | Sam   | 60000  | Null      |
+// | 4  | Max   | 90000  | Null      |
+// +----+-------+--------+-----------+
+// Output: 
+// +----------+
+// | Employee |
+// +----------+
+// | Joe      |
+// +----------+
+// Explanation: Joe is the only employee who earns more than his manager.
+select name from employee E
+where E.salary>(
+  select emp.salary from employee E
+  where emp.id=E.managerID
+)
 
-// Input: nums = [3,2,4], target = 6
-// Output: [1,2]
-// Example 3:
-
-// Input: nums = [3,3], target = 6
-// Output: [0,1]
-
-
-int sum =14;
-int arr[]={1,4,6,3,2,8,7,3};
-size_t=sizeof(arr)/sizeof(int);
-for(int i=0;i<size;i++){
-  for(int j=i;j<size;++j){
-    if(arr[i]+arr[j]==sum)
-      cout<<i<<j;
-  }
-}
-
-//Complexity O(n^2)
-
-Solution 2
-8 is the max number
-arr2 = {0,1,2,3,4,5,6,7,8}
-arr2[sum - arr[i]]  
-O(n) + O(n) + O(n)
-O(3n) => O(n)
-
-Solution 3
+select E.name from employee E
+join employee Emp
+on E.id = Emp.Managerid
+where E.salary>Emp.salary  
